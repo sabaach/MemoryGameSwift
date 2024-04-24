38aa770e46f1724db2d7cb45f9eb1877c1d52ebe
hungercodes <medeline17@gmail.com>
2024-04-25 03:01:29 +0700

    Initial Commit

---
+++ ColorMix/ColorMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixApp.swift
+//  ColorMix
+//
+//  Created by Medeline Agustine on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorMix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorMix
+//
+//  Created by Medeline Agustine on 25/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
