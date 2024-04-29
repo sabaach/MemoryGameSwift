cc4b2b0c517fea48b111bf8fb69f1ff51d46a6f2
FerrisWin <ferrisleroy127@gmail.com>
2024-04-29 12:19:42 +0700

    First Update

--- NanoChallenge 1/GameOver/GameOverScene.swift
+++ NanoChallenge 1/GameOver/GameOverScene.swift
@@ -15,53 +15,23 @@ class GameOverScene: SKScene {
         
     override func didMove(to view: SKView) {
         
+        let retryButton = self.childNode(withName: "//retry") as? SKSpriteNode
+        let failedStatus = self.childNode(withName: "//failed") as? SKSpriteNode
         
-        // Get label node from scene and store it for use later
-        let status = self.childNode(withName: "//status") as? SKSpriteNode
-        
-        let label = self.childNode(withName: "//label") as? SKLabelNode
-        
-        let retryNode = self.childNode(withName: "//retry") as! SKSpriteNode
-        
-        if win == false{
-//            status?.removeAllChildren()
-//            
-//            //Image kalau kalah
-//            let lossImage = SKSpriteNode(imageNamed: "Failed")
-//                
-//            //Position
-//            lossImage.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
-//                
-            //Add image to status node
-            self.addChild(lossImage)
-            
-            
-            label?.text = "Better Luck Next Time"
-        }
-        
-
     }
     
-    //Balik ke scene awal jika ingin main lagi
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let scene = GameScene(fileNamed: "GameScene")
-        let transition = SKTransition.crossFade(withDuration: 3)
-        scene!.scaleMode = .aspectFit
-        self.view?.presentScene(scene)
-    }
- 
-    
-
-    //PINDAH KE SCENE LAIN KETIKA GAME MAU BERUBAH SCENE (MISAL CONGRATULATIONS, GAME OVER)
-
-//        func endGame() {
-//            let transition = SKTransition.flipHorizontal(withDuration: 1.0)
-//            let gameOver = SKScene(fileNamed: "GameOverScene") as! GameOverScene
-//
-//            gameOver.scaleMode = .aspectFill
-//            gameOver.score = points
-//            view!.presentScene(gameOver, transition: transition)
-//        }
-    
+           for touch in touches {
+               let location = touch.location(in: self)
+               let node = atPoint(location)
+               
+               // Check if the retry button was touched
+               if node.name == "retry" {
+                   let scene = GameScene(fileNamed: "GameScene")
+                   scene!.scaleMode = .fill
+                   self.view?.presentScene(scene)
+               }
+           }
+       }
 }
 
--- NanoChallenge 1/GameScene/GameScene.swift
+++ NanoChallenge 1/GameScene/GameScene.swift
@@ -8,37 +8,36 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var characterTypes: [SKTexture] = []
+class GameScene: SKScene, SKPhysicsContactDelegate{
+    // SKPhysicsContactDelegate berguna untuk membaca dan menghandle physic contact di scenenya
+
     private var points : SKLabelNode?
     private var character : SKSpriteNode?
-    
-    let bombCategory:UInt32 = 0x1
+    private var characterShape: [SKTexture] = []
+    // Perbedaan value supaya program mendetect adanya tabrakan antar Object
+    // Penggunaan operasi bitwise untuk menggabungkan beberapa kategori dan membuat skenario tabrakan yang kompleks.
+
+    let bombCategory:UInt32 = 0x1  //Akan memiliki category bitMask '0x1'
     let pointCategory:UInt32 = 0x10
     let characterCategory:UInt32 = 0x100
     let groundCategory:UInt32 = 0x1000
+    
+    // Untuk melimit berapa banyak object yang jatuh supaya tidak terlalu banyak
+    private var limitAmount : Int = 0
+    
+    //Untuk menghitung jumlah points yang tercollect
+    private var pointsCollected : Int = 0
    
     
     override func didMove(to view: SKView) {
+        self.physicsWorld.contactDelegate = self
         
         // Get label node from scene and store it for use later
         self.points = self.childNode(withName: "//points") as? SKLabelNode
     
         //Character
         self.character = self.childNode(withName: "//character") as? SKSpriteNode
-        
-        
-        //Types ini akan dipakai untuk menyimpan bentuk character buat character di gamenya.
-        //Hanya bisa digunakkan di file "GameScene" karena menggunakan Acces Modifier "PRIVATE"
-        
-//        characterTypes.append(SKTexture(imageNamed: "Character"))
-//        characterTypes.append(SKTexture(imageNamed: "Explode"))
-//        
-//        let animation = SKAction.animate(with: characterTypes, timePerFrame: 0.2)
-//        let animationRepeat = SKAction.repeatForever(animation)
-//        character!.run(animationRepeat)
-        
+
         character?.physicsBody?.categoryBitMask = characterCategory
         character?.physicsBody?.contactTestBitMask = pointCategory | bombCategory
         
@@ -61,32 +60,119 @@ class GameScene: SKScene {
         // 1 = Bomb , 2 = point
         let random = Int.random(in: 1...2) //Ini akan membuat random menjatuhkan antara bomb / point
         let randomPosition1 = Int.random(in: -370...370) //Posisi pada GameScenenya (Sumbu X)
-        let randomPosition  = Int.random(in: 660...700) //Posisi pada GameScenenya (Sumbu y)
+        let randomPosition2  = Int.random(in: 660...700) //Posisi pada GameScenenya (Sumbu y)
         
+        var points = SKSpriteNode(imageNamed: "StarPixel")
+        points.position = CGPoint(x: randomPosition1, y: randomPosition2)
+        points.size = CGSize(width: 120, height: 120)
+        points.name = "point"
+        
+        points.physicsBody = SKPhysicsBody.init(circleOfRadius: 30)
+        points.physicsBody?.categoryBitMask = pointCategory
+        points.physicsBody?.contactTestBitMask = groundCategory | characterCategory
+        addChild(points)
+        
+        if (random == 1){
+            points.name = "bomb"
+            points.texture = SKTexture(imageNamed: "bomb")
+            points.physicsBody?.categoryBitMask = bombCategory
+        }
+        limitAmount += 1
+ 
     }
     
     //Memulai kerangka buat touch
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        //Disentuh dia akan bergeser ke arah tertentu
+        let touch = touches.first
+        if let location = touch?.location(in: self){
+        
+            if location.x > (character?.position.x)! / 2 {
+                character?.position.x += 30 //Berjalan sejauh skala 30 ke kanan
+            }
+            else if location.x < (character?.position.x)! / 2 {
+                character?.position.x -= 30 //Berlajan sejauh skala 30 ke kiri
+            }
+          
+        // Gyro
+          
+        }
+
      
     }
- 
     
+    func didBegin(_ contact: SKPhysicsContact) {
+         characterContact(contact)
+     }
+ 
+    //Function ketika character mengenai objek
+    func characterContact(_ contact: SKPhysicsContact) {
+        
+        let Body1 = contact.bodyA.node?.name
+        let Body2 = contact.bodyB.node?.name
+        
+        
+        if ((Body1 == "point") && (Body2 == "ground")) || ((Body1 == "bomb") && (Body2 == "ground")) {
+            //Ketika bomb ataupun point terkena ground, maka akan hilang objectnya, dengan func .removeFromParent()
+            contact.bodyA.node?.removeFromParent()
+            limitAmount -= 1
+        }
+        
+        else if ((Body1 == "ground") && (Body2 == "bomb")) || ((Body1 == "ground") && (Body2 == "point")) {
+            //Ketika bomb ataupun point terkena ground, maka akan hilang objectnya, dengan func .removeFromParent()
+            contact.bodyB.node?.removeFromParent()
+            limitAmount -= 1
+        }
+        
+        let collide: UInt32 = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+        if collide == characterCategory | pointCategory {
+            
+            pointsCollected += 1
+            points?.text = "\(pointsCollected)"
+            
+            if(Body1 == "point") {
+                contact.bodyA.node?.removeFromParent()
+            }
+            else {
+                contact.bodyB.node?.removeFromParent()
+            }
+           
+            if (pointsCollected == 3) {
+                let scene = GameOverScene(fileNamed: "GameWinScene")
+                scene!.scaleMode = .fill
+                let ending = SKTransition.push(with: .down, duration: 3.0)
+                self.view?.presentScene(scene!, transition: ending )
+            }
+            
+            
+        }
+        
+        else if collide == characterCategory | bombCategory {
+            
+            character?.texture = SKTexture(imageNamed: "Explode")
+            
+            //Call Scene
+            let scene = GameOverScene(fileNamed: "GameOverScene")
+            scene!.scaleMode = .fill
+            let ending = SKTransition.push(with: .down, duration: 3.0)
+            self.view?.presentScene(scene!, transition: ending )
+        }
+        
+
+    }
 
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered (Akan dipakai)
-      
+        
+        //Kalau amount yang jatuh totalnya dibawah 5 dia akan menjalankan function falling Object, tetapi kalau sudah, dia akan stop
+        if(limitAmount <= 3){
+            fallingObject()
+        }
+        
         
     }
-
-    //PINDAH KE SCENE LAIN KETIKA GAME MAU BERUBAH SCENE (MISAL CONGRATULATIONS, GAME OVER)
-
-//        func endGame() {
-//            let transition = SKTransition.flipHorizontal(withDuration: 1.0)
-//            let gameOver = SKScene(fileNamed: "GameOverScene") as! GameOverScene
-//            
-//            gameOver.scaleMode = .aspectFill
-//            gameOver.score = points
-//            view!.presentScene(gameOver, transition: transition)
-//        }
     
+    
+
 }
